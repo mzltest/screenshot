b0406ac3163dd296a5de0446210c27357e4c56d9
mzltest <52124882+mzltest@users.noreply.github.com>
2022-05-24 17:20:36 +0800

    Update index.ts

--- src/http/get-index/index.ts
+++ src/http/get-index/index.ts
@@ -1,26 +1,28 @@
-import { Request, Response } from '@vercel/node'
+let arc = require('@architect/functions')
+let parseBody = arc.http.helpers.bodyParser
 import { getScreenshot } from './_lib/puppeteer';
 
-module.exports = async (req: Request, res: Response) => {
+exports.handler = async function http (request) {
   const usage = "https://s.vercel.app/api?url=https://google.com&width=1280&height=720"
-  if (!req.query.url) return res.status(400).json({
-    "success": false,
-    "error": "No url query specified.",
-    "usage": usage
-  });
+  if (!request.queryStringParameters.url) return res.status(400).return  {
+    statusCode: 400,
+    headers: { 'content-type': 'image/png' },
+    body: {'ok':false,'err':'no url'}
   try {
-    const file = await getScreenshot(req.query.url, req.query.width, req.query.height);
-    res.setHeader('Content-Type', `image/png`);
+    const file = await getScreenshot(request.queryStringParameters.url, request.queryStringParameters.width, request.queryStringParameters.height);
+    /**res.setHeader('Content-Type', `image/png`);
     res.setHeader('Cache-Control', `public, immutable, no-transform, s-maxage=31536000, max-age=31536000`);
     res.status(200).end(file);
+    **/
+    return  {
+    statusCode: 200,
+    headers: { 'content-type': 'image/png' },
+    body: file
+  }
   } catch (error) {
     console.error(error)
-    res.setHeader('Content-Type', 'application/json');
-    res.status(400).json({
-      "success": false,
-      "error": "The server encountered an error. You may have inputted an invalid query.",
-      //"dev": error,
-      "usage": usage
-    });
+   return  {
+    statusCode: 400,
+    body: {'ok':false,'err':'err'}
   }
 }
